66482f5dfb60bdba1e19b5e870c4aee8c7f42aa3
tdeny96 <32493032+tdeny96@users.noreply.github.com>
2018-12-08 13:33:02 +0200

    Add files via upload

---
+++ SongActivity.java
@@ -0,0 +1,62 @@
+package deny.music;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SongActivity extends AppCompatActivity implements View.OnClickListener {
+    Button play,pause,stop,next;
+    MediaPlayer mp;
+    int pauseCurrentPosition;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_song);
+
+        play = (Button)findViewById(R.id.PlayButton);
+        pause = (Button)findViewById(R.id.PauseButton);
+        stop = (Button)findViewById(R.id.StopButton);
+        next = (Button)findViewById(R.id.NextButton);
+
+        play.setOnClickListener(this);
+        pause.setOnClickListener(this);
+        stop.setOnClickListener(this);
+        next.setOnClickListener(this);
+    }
+
+    public void onClick(View view){
+        switch(view.getId()){
+
+            case R.id.PlayButton:
+                if(mp==null) {
+                    mp = MediaPlayer.create(getApplicationContext(), R.raw.song);
+                    mp.start();
+                }
+                else if (!mp.isPlaying())
+                    mp.seekTo(pauseCurrentPosition);
+                mp.start();
+                break;
+            case  R.id.PauseButton:
+                if(mp==null) {
+                    mp.pause();
+                    pauseCurrentPosition = mp.getCurrentPosition();
+                }
+                break;
+            case R.id.StopButton:
+                if(mp!=null) {
+                    mp.stop();
+                    mp=null;
+                }
+                break;
+
+        }
+    }
+
+    public void GoToSongs(){
+        Intent intent1 = new Intent(SongActivity.this,MusicActivity.class);
+        startActivity(intent1);
+    }
+}
